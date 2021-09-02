30111488c361b02d9132cea7c6e6f8c80bed3280
Ivan <riplain@list.ru>
2021-09-02 23:18:53 +0600

    update pattern

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-# link = 'https://oauth.vk.com/authorize?client_id=7856174&display=page&scope=friends,photos,audio,video,stories,pages,notify,status,notes,wall,offline,docs,groups,email,market,stats,notifications&response_type=token&v=5.101&state=123456'
-
-api_version = 5.144
+api_version = 0
 
 group_token = ''
 
